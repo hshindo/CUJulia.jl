e2e7fc865f49e6d17eb88b56706131a877490a36
Hiroyuki Shindo <hshindo@outlook.com>
2017-02-24 18:25:24 +0900

    Update reducedim

--- src/reducedim.jl
+++ src/reducedim.jl
@@ -1,5 +1,84 @@
 import Base: reducedim, sum, maximum
 
+function reducedim_f{T}(::Type{T}, v0::String, op::String)
+    CuFunction("""
+    template<typename T>
+    __inline__ __device__ T warpReduce(T a) {
+        for (int delta = warpSize/2; delta > 0; delta /= 2) {
+            T b = __shfl_down(a, delta);
+            a = $op;
+        }
+        return a;
+    }
+    template<typename T>
+    __inline__ __device__ T blockReduce(T value) {
+        static __shared__ T temp[32]; // 32*32=1024
+        int laneId = threadIdx.y % warpSize;
+        int warpId = threadIdx.y / warpSize;
+
+        value = warpReduce<T>(value);
+        if (laneId == 0) temp[warpId] = value;
+        __syncthreads();
+
+        value = (laneId < 32) ? temp[laneId] : $v0;
+        value = warpReduce<T>(value);
+        return value;
+    }
+    __global__ void reduce(Array<$T,3> x, Array<$T,3> y) {
+        int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
+        int idx_y = blockIdx.y * blockDim.y + threadIdx.y;
+        int idx_z = blockIdx.z * blockDim.z + threadIdx.z;
+
+        $T a = (idx_y < x.dims[1]) ? x(idx_x,idx_y,idx_z) : $v0;
+        for (int i = idx_y+blockDim.y*gridDim.y; i < x.dims[1]; i += blockDim.y*gridDim.y) {
+            $T b = x(idx_x, i, idx_z);
+            a = $op;
+        }
+        a = blockReduce<$T>(a);
+        if (threadIdx.y == 0) y(blockIdx.x, blockIdx.y, blockIdx.z) = a;
+    }
+    """)
+end
+
+function reducedim(f::CuFunction, x::CuArray, dim::Int)
+    x3d = reshape3d(x, dim)
+    by = 1024
+    while true
+        gy = Int(ceil(size(x3d,2)/by))
+        y = similar(x, ntuple(i -> i==dim ? gy : size(x,i), ndims(x)))
+        y3d = reshape3d(y, dim)
+        f(x3d, y3d, dx=size(x3d,1), dy=size(x3d,2), dz=size(x3d,3), bx=1, by=by, bz=1)
+        gy == 1 && return y
+        x3d = y3d
+    end
+end
+
+@generated function maximum{T}(x::CuArray{T}, dim::Int)
+    f = reducedim_f(T, "-1e10", "a > b ? a : b")
+    quote
+        reducedim($f, x, dim)
+    end
+end
+
+@generated function sum{T}(x::CuArray{T}, dim::Int)
+    f = reducedim_f(T, "0", "a + b")
+    quote
+        reducedim($f, x, dim)
+    end
+end
+
+function reshape3d(x::CuArray, dim::Int)
+    dim1, dim2, dim3 = 1, size(x,dim), 1
+    for i = 1:dim-1
+        dim1 *= size(x,i)
+    end
+    for i = dim+1:ndims(x)
+        dim3 *= size(x,i)
+    end
+    reshape(x, dim1, dim2, dim3)
+end
+
+#=
 function reducedim_f{T}(::Type{T}, op::String)
     blocksize = 1024
     CuFunction("""
@@ -19,6 +98,7 @@ function reducedim_f{T}(::Type{T}, op::String)
         }
 
         temp[tid] = a;
+        $T b;
         __syncthreads();
 
         if (($blocksize >= 512) && (tid < 256)) {
@@ -49,82 +129,4 @@ function reducedim_f{T}(::Type{T}, op::String)
         if (threadIdx.y == 0) y(blockIdx.x, blockIdx.y, blockIdx.z) = a;
     }""")
 end
-
-function reducedim_f2{T}(::Type{T}, op::String)
-    CuFunction("""
-    template<typename T>
-    __inline__ __device__ T warpReduce(T a) {
-        for (int delta = warpSize/2; delta > 0; delta /= 2) {
-            T b = __shfl_down(a, delta);
-            a = $op;
-        }
-        return a;
-    }
-    template<typename T>
-    __inline__ __device__ T blockReduce(T value) {
-        static __shared__ T temp[32];
-        int laneId = threadIdx.y % warpSize;
-        int warpId = threadIdx.y / warpSize;
-
-        value = warpReduce<T>(value);
-        if (laneId == 0) temp[warpId] = value;
-        __syncthreads();
-
-        value = (laneId < 32) ? temp[laneId] : 0;
-        value = warpReduce<T>(value);
-        return value;
-    }
-    __global__ void reduce(Array<$T,3> x, Array<$T,3> y) {
-        int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
-        int idx_y = blockIdx.y * blockDim.y + threadIdx.y;
-        int idx_z = blockIdx.z * blockDim.z + threadIdx.z;
-        if (idx_y >= x.dims[1]) return;
-
-        $T a = x(idx_x, idx_y, idx_z);
-        for (int i = idx_y+blockDim.y*gridDim.y; i < x.dims[1]; i += blockDim.y*gridDim.y) {
-            $T b = x(idx_x, i, idx_z);
-            a = $op;
-        }
-        a = blockReduce<$T>(a);
-        if (threadIdx.y == 0) y(blockIdx.x, blockIdx.y, blockIdx.z) = a;
-    }
-    """)
-end
-
-function reducedim(f::CuFunction, x::CuArray, dim::Int)
-    x3d = reshape3d(x, dim)
-    by = 1024
-    while true
-        gy = Int(ceil(size(x3d,2)/by))
-        y = similar(x, ntuple(i -> i==dim ? gy : size(x,i), ndims(x)))
-        y3d = reshape3d(y, dim)
-        f(x3d, y3d, dx=size(x3d,1), dy=size(x3d,2), dz=size(x3d,3), bx=1, by=by, bz=1)
-        gy == 1 && return y
-        x3d = y3d
-    end
-end
-
-@generated function maximum{T}(x::CuArray{T}, dim::Int)
-    f = reducedim_f(T, "a > b ? a : b")
-    quote
-        reducedim($f, x, dim)
-    end
-end
-
-@generated function sum{T}(x::CuArray{T}, dim::Int)
-    f = reducedim_f(T, "a + b")
-    quote
-        reducedim($f, x, dim)
-    end
-end
-
-function reshape3d(x::CuArray, dim::Int)
-    dim1, dim2, dim3 = 1, size(x,dim), 1
-    for i = 1:dim-1
-        dim1 *= size(x,i)
-    end
-    for i = dim+1:ndims(x)
-        dim3 *= size(x,i)
-    end
-    reshape(x, dim1, dim2, dim3)
-end
+=#
