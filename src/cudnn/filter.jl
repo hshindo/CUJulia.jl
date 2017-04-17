24c4aaa1ad36443a3f34712e9d290c8914d6d47f
Hiroyuki Shindo <hshindo@outlook.com>
2017-04-18 07:52:19 +0900

    Fix filter

--- src/cudnn/filter.jl
+++ src/cudnn/filter.jl
@@ -1,13 +1,16 @@
 type FilterDesc
     ptr::Ptr{Void}
 
-    function FilterDesc{T,N}(x::CuArray{T,N})
-        csize = Cint[size(x,i) for i=N:-1:1]
+    function FilterDesc(x; format=CUDNN_TENSOR_NCHW)
         p = Ptr{Void}[0]
         cudnnCreateFilterDescriptor(p)
-        cudnnSetFilterNdDescriptor(p[1], datatype(T), format, N, csize)
         desc = new(p[1])
         finalizer(desc, cudnnDestroyFilterDescriptor)
+
+        T = eltype(x)
+        N = ndims(x)
+        c_size = Cint[size(x,i) for i=N:-1:1]
+        cudnnSetFilterNdDescriptor(desc, datatype(T), format, N, c_size)
         desc
     end
 end
