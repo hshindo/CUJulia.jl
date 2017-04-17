2555eec7e13d0ad4b27caec3fb8f4aa6472c2756
Hiroyuki Shindo <hshindo@outlook.com>
2017-04-18 07:51:36 +0900

    Fix activation

--- src/cudnn/activation.jl
+++ src/cudnn/activation.jl
@@ -3,22 +3,35 @@ export
     CUDNN_ACTIVATION_SIGMOID,
     CUDNN_ACTIVATION_RELU,
     CUDNN_ACTIVATION_TANH,
-    CUDNN_ACTIVATION_CLIPPED_RELU,
-
-    cudnnActivationForward,
-    cudnnActivationBackward
+    CUDNN_ACTIVATION_CLIPPED_RELU
 
 type ActivationDesc
     ptr::Ptr{Void}
+end
 
-    function ActivationDesc(mode::UInt32; relu_nanopt=CUDNN_NOT_PROPAGATE_NAN, relu_ceiling=1.0)
-        p = Ptr{Void}[0]
-        cudnnCreateActivationDescriptor(p)
-        cudnnSetActivationDescriptor(p[1], mode, relu_nanopt, relu_ceiling)
-        desc = new(p[1])
-        finalizer(desc, cudnnDestroyActivationDescriptor)
-        desc
-    end
+function ActivationDesc()
+    p = Ptr{Void}[0]
+    cudnnCreateActivationDescriptor(p)
+    desc = ActivationDesc(p[1])
+    finalizer(desc, cudnnDestroyActivationDescriptor)
+    desc
 end
 
 Base.unsafe_convert(::Type{Ptr{Void}}, desc::ActivationDesc) = desc.ptr
+
+function activation(mode, x; relu_nanopt=CUDNN_NOT_PROPAGATE_NAN, relu_ceiling=1.0)
+    actdesc = ActivationDesc()
+    cudnnSetActivationDescriptor(actdesc, mode, relu_nanopt, relu_ceiling)
+
+    h = handle(x)
+    T = eltype(x)
+    xdesc = TensorDesc(x)
+    y = similar(x)
+    cudnnActivationForward(h, actdesc, T[1], xdesc, x, T[0], xdesc, y)
+
+    function backward!(gy, gx)
+        isvoid(gx) && return
+        cudnnActivationBackward(h, actdesc, T[1], xdesc, y, xdesc, gy, xdesc, x, T[1], xdesc, gx)
+    end
+    y, backward!
+end
